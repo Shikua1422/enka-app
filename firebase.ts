4455fa6df45723203613288e531ac7397dc53c2e
Shikua <c1004120@planet.kanazawa-it.ac.jp>
2026-05-14 02:19:24 +0900

    add profile fetch

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDAr3oCOWKW9GHmgwqXwcsrCb4J24cg7UI",
   authDomain: "enka-app-8993c.firebaseapp.com",
   projectId: "enka-app-8993c",
-  storageBucket: "enka-app-8993c.firebasestorage.app",
+
+  // 🔥ここを修正
+  storageBucket: "enka-app-8993c.appspot.com",
+
   messagingSenderId: "458595216039",
   appId: "1:458595216039:web:35f640bcd88a2347e94982",
   measurementId: "G-1T00BJKX40"
